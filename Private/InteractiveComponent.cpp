e4923309fb14dcfe44b6edfb9445ecff7704c8d0
BuildTools <unconfigured@null.spigotmc.org>
2024-02-03 02:54:56 +0800

    ASN666

--- Private/InteractiveComponent.cpp
+++ Private/InteractiveComponent.cpp
@@ -8,12 +8,11 @@
 // Sets default values for this component's properties
 UInteractiveComponent::UInteractiveComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+	
 	PrimaryComponentTick.bCanEverTick = true;
 	RayCastLength = 1500.f;
 	RayCastRadius = 5.f;
-	// ...
+	
 }
 
 
@@ -22,7 +21,7 @@ void UInteractiveComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	
 	
 }
 
