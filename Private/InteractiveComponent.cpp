8de854d02499e62912dd2fa4fd593c6467189493
BuildTools <unconfigured@null.spigotmc.org>
2024-02-03 02:56:04 +0800

    ASN666

--- Private/InteractiveComponent.cpp
+++ Private/InteractiveComponent.cpp
@@ -1,11 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "InteractiveComponent.h"
 #include "CPPComponent/Public/InteractiveComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 
-// Sets default values for this component's properties
 UInteractiveComponent::UInteractiveComponent()
 {
 	
@@ -16,7 +14,6 @@ UInteractiveComponent::UInteractiveComponent()
 }
 
 
-// Called when the game starts
 void UInteractiveComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -26,7 +23,6 @@ void UInteractiveComponent::BeginPlay()
 }
 
 
-// Called every frame
 void UInteractiveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
